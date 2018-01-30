12e69d0f69e99282fc8d96933e03b7454c918592
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-30 15:19:14 +0200

    trecia uzduotis

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-	// write your code here
+        System.out.println("Iveskite du skaicius");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int Suma = a + b;
+        int Skirtumas = a - b;
+
+        System.out.println("Skaiciu suma: " + Suma);
+        System.out.println("Skaiciu skirtumas: " + Skirtumas);
+
+
+        // write your code here
     }
 }
